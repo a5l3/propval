4cc1956517527cb7f8565f14776cbe17032a31fe
Anchorlink <sashnortier@gmail.com>
2018-11-03 14:49:39 +0200

    S14L181 - Custom Value Parsing

--- load-csv.js
+++ load-csv.js
@@ -1,7 +1,7 @@
 const fs = require('fs'); 
 const _ = require('lodash'); 
 // S14L177
-function loadCSV(filename, options) {
+function loadCSV(filename, { converters = {} }) {                                           // S14L181 set converts to an empty obj
     let data = fs.readFileSync(filename, { encoding: 'utf-8'}); 
     data = data.split('\n').map(row => row.split(','))                                      // S14L178 splitting on each new line we return an array for each row 
     data = data.map(row => _.dropRightWhile(row, val => val === ''))                        // S14L179
@@ -13,6 +13,11 @@ function loadCSV(filename, options) {
             return row
         }
         return row.map((element, index) => {
+            if(converters[headers[index]]) {                                                 // S14L181
+                const converted = converters[headers[index]](element)
+                return _.isNaN(converted) ? element : converted
+            }           
+
             const result = parseFloat(element)
             return _.isNaN(result) ? element : result 
         })
@@ -21,4 +26,8 @@ function loadCSV(filename, options) {
     console.log(data)
 }
 
-loadCSV('data.csv');
+loadCSV('data.csv', {
+    converters: {
+        passed: val => (val === 'TRUE' ? true : false)                                          // S14L181 the key is the name of the header that will be passed this custom func 
+    }
+});
