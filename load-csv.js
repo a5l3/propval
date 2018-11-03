ec47da2e21166cc23ccdc83520b9fb7ee13aeba8
Anchorlink <sashnortier@gmail.com>
2018-11-03 13:03:25 +0200

    S14L179 - Dropping Trailing Columns

--- load-csv.js
+++ load-csv.js
@@ -1,10 +1,12 @@
 const fs = require('fs'); 
-
+const _ = require('lodash'); 
 // S14L177
 function loadCSV(filename, options) {
-    const data = fs.readFileSync(filename, { encoding: 'utf-8'}); 
-    console.log(data.split('\n').map(row => row.split(',')))              // S14L178 splitting on each new line we return an array for each row 
-    
+    let data = fs.readFileSync(filename, { encoding: 'utf-8'}); 
+    data = data.split('\n').map(row => row.split(','))                                      // S14L178 splitting on each new line we return an array for each row 
+    data = data.map(row => _.dropRightWhile(row, val => val === ''))                        // S14L179
+
+    console.log(data);
 }
 
 loadCSV('data.csv');
