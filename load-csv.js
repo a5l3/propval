25d55abf1141ed3fac55832ebc86ba1eea11c7d8
Anchorlink <sashnortier@gmail.com>
2018-11-03 10:52:02 +0200

    S14L178 - Splitting into Columns

--- load-csv.js
+++ load-csv.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 // S14L177
 function loadCSV(filename, options) {
     const data = fs.readFileSync(filename, { encoding: 'utf-8'}); 
-    console.log(data)
+    console.log(data.split('\n').map(row => row.split(',')))              // S14L178 splitting on each new line we return an array for each row 
+    
 }
 
 loadCSV('data.csv');
