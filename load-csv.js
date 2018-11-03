92bb361adcaff0b0de5b989b5d20ba94ef10c355
Anchorlink <sashnortier@gmail.com>
2018-11-03 14:33:31 +0200

    S14L180 - Parsing Numerical Values

--- load-csv.js
+++ load-csv.js
@@ -5,8 +5,20 @@ function loadCSV(filename, options) {
     let data = fs.readFileSync(filename, { encoding: 'utf-8'}); 
     data = data.split('\n').map(row => row.split(','))                                      // S14L178 splitting on each new line we return an array for each row 
     data = data.map(row => _.dropRightWhile(row, val => val === ''))                        // S14L179
+    const headers = _.first(data)                                                            // S14L180
+    
 
-    console.log(data);
+    data = data.map((row, index) => {    // S14L180
+        if(index === 0){
+            return row
+        }
+        return row.map((element, index) => {
+            const result = parseFloat(element)
+            return _.isNaN(result) ? element : result 
+        })
+    }); 
+
+    console.log(data)
 }
 
 loadCSV('data.csv');
