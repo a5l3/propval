65a25fbccbc8f7b0078e7bfcf73be48f661ae8a2
Anchorlink <sashnortier@gmail.com>
2018-11-04 17:17:51 +0200

    knn algorithm implementation

---
+++ index.js
@@ -0,0 +1,75 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs'); 
+const loadCSV = require('./load-csv.js'); 
+
+// S4L53
+function knn(features, labels, predictionPoint, k){
+    // S4L57
+    const {mean, variance} = tf.moments(features, 0); 
+    const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(0.5)); 
+
+    
+    return features
+        .sub(mean)
+        .div(variance.pow(0.5))
+        .sub(scaledPrediction)
+        .pow(2)
+        .sum(1)
+        .pow(0.5)
+        .expandDims(1)
+        .concat(labels, 1)
+        .unstack()
+        .sort((a,b) => a.get(0) > b.get(0) ? 1 : -1)
+        .slice(0, k)
+        .reduce((acc, pair) => acc + pair.get(1), 0) / k; 
+}
+
+
+// S4L52
+let {
+    features,                   // rest of features 
+    labels,                     // rest of labels 
+    testFeatures,               // 10 features 
+    testLabels                  // 10 labels 
+    } = loadCSV('kc_house_data.csv', {
+    shuffle: true,                  // we need to shuffle data when dealing with KNN, avoids bias 
+    splitTest: 10,                  // two datasets, test and training, 10 records for testing the rest for training 
+    dataColumns: [
+        'date','bedrooms','bathrooms','sqft_living','sqft_lot','floors','view','condition','grade','sqft_above','sqft_basement','yr_built','yr_renovated','zipcode','lat','long','sqft_living15','sqft_lot15'
+    ],   // specifying the columns we want 
+    labelColumns: ['price']         // specifying our label data
+}); 
+
+// S4L53 
+features = tf.tensor(features); 
+labels = tf.tensor(labels);
+
+
+// S4L54 
+testFeatures.forEach((testPoint, i) => {
+    const result = knn(features, labels, tf.tensor(testPoint), 10);
+    const err = (testLabels[i][0] - result) / testLabels[i][0]; 
+    console.log('Error percent:', err * 100, ' Result:', result, 'Actual:', testLabels[i][0]);
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log('Test Features: \n', testFeatures);
+// console.log('Test Labels: \n', testLabels);
--- load-csv.js
+++ load-csv.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 const _ = require('lodash');
 const shuffleSeed = require('shuffle-seed');
 
-
-// S14L182 helper function for dataColumns & labelColumns, extract column names from data set 
 function extractColumns(data, columnNames) {
   const headers = _.first(data);
 
@@ -13,77 +11,77 @@ function extractColumns(data, columnNames) {
   return extracted;
 }
 
-// S14L177
-function loadCSV(
+module.exports = function loadCSV(
   filename,
   {
-    converters = {},
     dataColumns = [],
     labelColumns = [],
-    shuffle = true,
+    converters = {},
+    shuffle = false,
     splitTest = false
   }
-) {                                                                                         // S14L181 set converts to an empty obj
+) {
   let data = fs.readFileSync(filename, { encoding: 'utf-8' });
-  data = data.split('\n').map(row => row.split(','));                                       // S14L178 splitting on each new line we return an array for each row
-  data = data.map(row => _.dropRightWhile(row, val => val === ''));                         // S14L179
-  const headers = _.first(data);                                                             // S14L180
+  data = _.map(data.split('\n'), d => d.split(','));
+  data = _.dropRightWhile(data, val => _.isEqual(val, ['']));
+  const headers = _.first(data);
 
-  data = data.map((row, index) => {                                                         // S14L180
+  data = _.map(data, (row, index) => {
     if (index === 0) {
       return row;
     }
-
-    return row.map((element, index) => {
-      if (converters[headers[index]]) {                                                     // S14L181
+    return _.map(row, (element, index) => {
+      if (converters[headers[index]]) {
         const converted = converters[headers[index]](element);
         return _.isNaN(converted) ? element : converted;
       }
 
-      const result = parseFloat(element);
+      const result = parseFloat(element.replace('"', ''));
       return _.isNaN(result) ? element : result;
     });
   });
 
-  let labels = extractColumns(data, labelColumns);                                          // S14L182
+  let labels = extractColumns(data, labelColumns);
   data = extractColumns(data, dataColumns);
 
   data.shift();
   labels.shift();
 
-  if (shuffle) {                                                                             // S14L183 
-    data = shuffleSeed.shuffle(data, 'phrase');
-    labels = shuffleSeed.shuffle(labels, 'phrase');
+  if (shuffle) {
+    data = shuffleSeed.shuffle(data, 'hakunamatata');
+    labels = shuffleSeed.shuffle(labels, 'hakunamatata');
   }
 
-  if (splitTest) {                                                                          // S14L182
+  if (splitTest) {
     const trainSize = _.isNumber(splitTest)
       ? splitTest
       : Math.floor(data.length / 2);
 
     return {
-      features: data.slice(0, trainSize),
-      labels: labels.slice(0, trainSize),
-      testFeatures: data.slice(trainSize),
-      testLabels: labels.slice(trainSize)
+      features: data.slice(trainSize),
+      labels: labels.slice(trainSize),
+      testFeatures: data.slice(0, trainSize),
+      testLabels: labels.slice(0, trainSize)
     };
   } else {
     return { features: data, labels };
   }
-}
+};
 
-const { features, labels, testFeatures, testLabels } = loadCSV('data.csv', {
-  dataColumns: ['height', 'value'],
-  labelColumns: ['passed'],
-  shuffle: true,
-  splitTest: true,
-  converters: {
-    passed: val => (val === 'TRUE' ? 1 : 0)
-  }
-});
 
-console.log('propval:')
-console.log('Features', features);
-console.log('Labels', labels);
-console.log('testFeatures', testFeatures);
-console.log('testLabels', testLabels);
+// Commented out, remove module export and uncomment.
+// const { features, labels, testFeatures, testLabels } = loadCSV('data.csv', {
+//   dataColumns: ['height', 'value'],
+//   labelColumns: ['passed'],
+//   shuffle: true,
+//   splitTest: true,
+//   converters: {
+//     passed: val => (val === 'TRUE' ? 1 : 0)
+//   }
+// });
+
+// console.log('propval:')
+// console.log('Features', features);
+// console.log('Labels', labels);
+// console.log('testFeatures', testFeatures);
+// console.log('testLabels', testLabels);
